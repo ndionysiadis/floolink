0d6fa5a0acd448da3fbde6405292f3347fe12a74
ndionysiadis <ndionysiadis@outlook.com>
2025-03-19 23:30:39 +0200

    package updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import motion from 'tailwindcss-motion';
-import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette';
 
 /** @type {import('tailwindcss').Config} */
 export default {
