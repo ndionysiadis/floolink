02673b41710c8c7d98c06d193a3bb048dd7e424e
Nicolas Dionysiadis <136641645+ndionysiadis@users.noreply.github.com>
2025-06-05 23:01:21 +0300

    fix: clean up controller and repository

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,19 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        {
-            return Inertia::render('Index', [
-                'laravelVersion' => Application::VERSION,
-                'phpVersion' => PHP_VERSION,
-                'generatedLink' => session('generatedLink')
-            ]);
-        }
+        return Inertia::render('Index', [
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+            'generatedLink' => session('generatedLink')
+        ]);
     }
 }
